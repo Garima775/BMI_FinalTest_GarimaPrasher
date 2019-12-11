3c7fb12665ad2e7ad95eddc9a04d190d2381aebe
Garima <garimaprashar775@gmail.com>
2019-12-11 18:31:31 -0500

    Initial Commit

---
+++ BMI_FinalTest_GarimaPrasher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI_FinalTest_GarimaPrasher
+//
+//  Created by Garima Prasher on 2019-12-11.
+//  Copyright © 2019 Garima Prasher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
