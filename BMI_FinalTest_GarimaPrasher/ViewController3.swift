01a5d1c755a37fd871710c7747e14b2e71bf7d89
Garima <garimaprashar775@gmail.com>
2019-12-13 15:34:39 -0500

    adding functionality of update button

--- BMI_FinalTest_GarimaPrasher/ViewController3.swift
+++ BMI_FinalTest_GarimaPrasher/ViewController3.swift
@@ -1,30 +1,60 @@
 //
-//  ViewController3.swift
+//  
 //  BMI_FinalTest_GarimaPrasher
+//File Name: Viewcontroller.swift
+//Author Name: Garima Prasher
+//student id: 301093329
+//DATE : 11 Dec 2019
 //
 //  Created by Garima Prasher on 2019-12-13.
 //  Copyright © 2019 Garima Prasher. All rights reserved.
 //
 
 import UIKit
+import Firebase
 
 class ViewController3: UIViewController {
-
+    
+    
+    @IBOutlet weak var weightupdate: UITextField!
+    
+    @IBOutlet weak var bmiupdate: UITextField!
+    
+    var dict = [String:AnyObject]()
+    
+    var db:Firestore?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+    @IBAction func Update(_ sender: UIButton) {
+        
+        db = Firestore.firestore()
+            
 
-    /*
-    // MARK: - Navigation
+            let parameters = ["Weight":weightupdate.text!,"BMI":bmiupdate.text!,"docId":dict["docId"]!] as [String : Any]
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+             db?.collection("data").document((dict["docId"] as? String)!).updateData(parameters as [String : Any]){
+                 err in
+                 if let error = err{
+                     print(error.localizedDescription)
+                     
+                 }else{
+                     print("document added successfully")
+                     
+                     let alert = UIAlertController(title: "Message", message: "Successfully Updated", preferredStyle: .alert)
+                     let okay = UIAlertAction(title: "OK", style: .default, handler: { (action) in
+                         self.navigationController?.popViewController(animated: true)
+                     })
+                     alert.addAction(okay)
+                     self.present(alert, animated: true, completion: nil)
+                     
+                     
+                 }
+
+             }
+        }
+        
     }
-    */
-
-}
+    
